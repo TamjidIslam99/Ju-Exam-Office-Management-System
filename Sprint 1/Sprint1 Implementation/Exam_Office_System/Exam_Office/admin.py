2cb9bc651933e9c40bda66fe059d4927c1221ac5
TamjidIslam99 <islamtamjid276@gmail.com>
2024-10-28 17:30:20 +0600

    feature:Added Models.py and admin.py

--- Sprint 1/Sprint1 Implementation/Exam_Office_System/Exam_Office/admin.py
+++ Sprint 1/Sprint1 Implementation/Exam_Office_System/Exam_Office/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import (
+    User,
+    Department,
+    Student,
+    Teacher,
+    ExamOfficeOrAdmin,
+    Course,
+    Exam,
+    ExamSchedule,
+    ExamRegistration,
+    Result,
+    MarksheetApplication,
+    CertificateApplication,
+    TeacherRemuneration,
+    ExamMaterial,
+    Attendance
+)
 
-# Register your models here.
+# Register each model with the admin site
+admin.site.register(User)
+admin.site.register(Department)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(ExamOfficeOrAdmin)
+admin.site.register(Course)
+admin.site.register(Exam)
+admin.site.register(ExamSchedule)
+admin.site.register(ExamRegistration)
+admin.site.register(Result)
+admin.site.register(MarksheetApplication)
+admin.site.register(CertificateApplication)
+admin.site.register(TeacherRemuneration)
+admin.site.register(ExamMaterial)
+admin.site.register(Attendance)
