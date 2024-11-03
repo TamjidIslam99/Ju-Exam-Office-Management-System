296f3b61e872fb492e62f66dd300fbaa6fb90034
Nahid-29 <kamrulhasannahid2002@gmail.com>
2024-11-03 23:00:55 +0600

    Folder name changed

---
+++ Sprint/Sprint Implementation/Exam_Office_System/myproject/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+import django
+
+sys.path.insert(0, os.path.abspath('../'))  # Adjust path to your Django project root if needed
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
+django.setup()
+
+project = 'Exam_Office'
+copyright = '2024, Nahid'
+author = 'Nahid'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'python'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
